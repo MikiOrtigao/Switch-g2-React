9419bc6bb5e06e4c1eb2ece9bb8f110ec1be0d63
Cárina <carina.alas11@gmail.com>
2019-06-01 19:45:06 +0100

    Starting to implement us250

--- src/views/Room/RoomConfiguration/US250.js
+++ src/views/Room/RoomConfiguration/US250.js
@@ -2,47 +2,54 @@ import React, { Component } from 'react';
 import {Collapse, Button, CardBody, Card, Form, FormGroup, Label, Input} from 'reactstrap';
 import US108Select from "./US108Select";
 
-class US250 extends Component {
-  constructor(props) {
-    super(props);
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      collapse: false,
-    };
-  }
 
-  componentDidMount(id) {
-    fetch('http://localhost:9898/roomConfiguration/rooms/'+{id}+'/sensors')
-      .then(res => res.json())
-      .then((json) => {
-        this.setState({
-          isLoaded: true,
+  class US250 extends Component {
+
+
+    constructor(props) {
+      super(props);
+      this.toggle = this.toggle.bind(this);
+      this.state = {
+        item: [],
+        isLoaded: false,
+      }
+    }
+
+    componentDidMount() {
+      fetch('http://localhost:9898/roomConfiguration/rooms/B107/sensors')
+        .then(res => res.json())
+        .then((json) => {
+          this.setState({
+            isLoaded: true,
+            item: json,
+          })
         })
-      })
-      .catch(console.log)
-  }
+        .catch(console.log)
+    }
 
-  toggle() {
-    this.setState(state => ({ collapse: !state.collapse }));
-  }
+    toggle() {
+      this.setState(state => ({collapse: !state.collapse}));
+    }
+
+
 
   getSensorsInARoom(){
     return (
       <div>
 
-        {/*<ul>*/}
-          {/*{item.map(item => (*/}
-            {/*<li key={item.name}>*/}
-              {/*Name: {item.name}*/}
-            {/*</li>*/}
-          {/*))}*/}
-        {/*</ul>*/}
-
+        <ul>
+          {item.map(item => (
+            <li key={item.name}>
+              Name: {item.name}
+            </li>
+          ))}
+        </ul>
       </div>
     )
   }
 
   render() {
+    var {id, item} = this.state;
     return (
       <div>
         <Button onClick={this.toggle} style={{ backgroundColor: '#FFFFFF', marginBottom: '1rem' }}>I want to get a list of all sensors in a room, so that I can configure them. (US250)</Button>
@@ -52,10 +59,11 @@ class US250 extends Component {
               <Form action="" method="post">
                 <FormGroup>
                   <Label>Select Room</Label>
-                  <US108Select/>
+                  <US108Select name={id}/>
                 </FormGroup>
               </Form>
-              <Button onClick={this.getSensorsInARoom} block color="light">Get list of sensors</Button>
+              <Button onClick={this.getSensorsInARoom()} block color="light">Get list of sensors</Button>
+
 
             </CardBody>
           </Card>
@@ -63,6 +71,7 @@ class US250 extends Component {
       </div>
     );
   }
+
 }
 
 export default US250;
