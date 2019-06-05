d3ac44377a3ad5b3e7ea49f8347f6e69e1cecbf9
Dark <1181722@isep.ipp.pt>
2019-06-05 11:21:03 +0100

    Changed us149 into it's own folder

--- src/views/House/HouseConfiguration/Example.js
+++ src/views/House/HouseConfiguration/Example.js
@@ -4,7 +4,7 @@ import US109Redux from './US109Redux/US109Redux'
 import US130 from './US130'
 import US145 from './US145/US145'
 import US147 from './US147/US147'
-import US149 from './US149'
+import US149 from './US149/US149'
 import US105 from "./US105";
 
 
